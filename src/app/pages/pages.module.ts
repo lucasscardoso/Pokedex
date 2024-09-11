fb26a779088fe73ac42e275fd8ef111b1b6ae301
lucasscardoso <gajeel.red.fox@hotmai.com>
2024-09-11 14:33:15 -0300

    criados novos componentes, details e home, adicionado scss na pasta config-scss, editado o style.scss, criado novo module em pages

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//pages
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
+import { DetailsComponent } from './details/details.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule
+
+  ]
+})
+export class PagesModule { }
