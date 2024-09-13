2cdf91155e7bfb474cc2f31dc65bb15cd7726f5a
lucasscardoso <gajeel.red.fox@hotmai.com>
2024-09-13 15:39:00 -0300

    alterado details, html e ts, criado funcao de get para capturar os pokemons pelo id utilizando activateroute e forkjoin, retornando os dois resultados selecionados, testes realizados via console log e esta funcional

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+
+//services
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -7,4 +12,25 @@ import { Component } from '@angular/core';
 })
 export class DetailsComponent {
 
+  private urlPokemon:string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName:string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+
+  constructor(private activatedRoute:ActivatedRoute,private pokeApiService:PokeApiService){}
+
+  ngOnInit(){
+    this.pokemon
+  }
+
+  get pokemon(){
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon,name]).subscribe(
+      res => {
+        console.log(res)
+      }
+    );
+  }
 }
